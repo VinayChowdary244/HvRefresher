e66be729f9472c68674e3c54d969c27d36c9a474
Vinay Penumaka <84056806+VinayChowdary244@users.noreply.github.com>
2022-12-09 10:40:49 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,12 @@
+students = ['rahul','ram','raj','ravi','raghu','rani','reshma','sagar']
+print('The student names are :')
+for s in students:
+    print(s)
+l = [1 , 2 , 3.5 , 4.5 , 'aditya' , True ]
+print(l)
+print(l[0])
+print(l[4])
+print(l[1:3]) #include 1 and exclude 3
+print(l[1:5])
+print(l[2:])
+print(l*2)    
---
+++ dict.py
@@ -0,0 +1,16 @@
+details = { 'ram' : 90, 'rahul' : 80, 'satya' : 50, 'anik' : 100 }
+details[4]= "Test"
+print(details)
+print(details.values())
+print(details.keys())
+print(details[4])
+print(details)
+dict = {
+    "college" : "aditya",
+    "location" : "andhra pradesh",
+    "stream" : "ece"
+}
+for info in dict:
+    print(info)
+for info1 in dict:
+    print(dict[info1])
